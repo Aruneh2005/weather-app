712fed5fb54ac485cdbce3c3fdaf3c4d7ae46493
Arunkarthik A <a4un2005@gmail.com>
2025-06-23 12:14:06 +0530

    Add files via upload

---
+++ get-weather/src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+import { TiWeatherSunny } from "react-icons/ti";
+
+function Header() {
+  return (
+    <header>
+      <div className='container ui block header'>
+        <h1 className='title'>
+          GET WEATHER{" "}
+          <span className='logo'>
+            <TiWeatherSunny />
+          </span>
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
